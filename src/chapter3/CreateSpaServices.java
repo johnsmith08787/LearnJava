0c0f45603df2170cf40567970153a7f997c408f2
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-06 12:22:24 -0500

    Chapter 3 - with debug files

--- src/chapter3/CreateSpaServices.java
+++ src/chapter3/CreateSpaServices.java
@@ -14,19 +14,20 @@ import java.util.Scanner;
 public class CreateSpaServices {
 
     public static void main(String[] args) {
-        String service;
-        double price;
+        
+//        String service;
+//        double price;
         
         SpaService firstService = new SpaService();
         SpaService secondService = new SpaService();
-        Scanner keyboard = new Scanner(System.in);
+//        Scanner keyboard = new Scanner(System.in);
         
-        System.out.print("Enter service: ");
-        service = keyboard.nextLine();
-        System.out.print("Enter price: ");
-        price = keyboard.nextDouble();
-        firstService.setServiceDescription(service);
-        firstService.setPrice(price);
+//        System.out.print("Enter service: ");
+//        service = keyboard.nextLine();
+//        System.out.print("Enter price: ");
+//        price = keyboard.nextDouble();
+//        firstService.setServiceDescription(service);
+//        firstService.setPrice(price);
         
 //        keyboard.nextLine();
 //        System.out.print("Enter service: ");
@@ -36,11 +37,32 @@ public class CreateSpaServices {
 //        secondService.setServiceDescription(service);
 //        secondService.setPrice(price);
         
+        firstService = getData(firstService);
+        secondService = getData(secondService);
+        
         System.out.println("First service details: ");
             System.out.println(firstService.getServiceDescription() + " $" + firstService.getPrice());
         
         System.out.println("Second service details: ");
             System.out.println(secondService.getServiceDescription() + " $" + secondService.getPrice());
+    }
+    
+    public static SpaService getData(SpaService s){
+        String service;
+        double price;
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Enter service >> ");
+        service = keyboard.nextLine();
+        System.out.print("Enter price >> ");
+        price = keyboard.nextDouble();
+        keyboard.nextLine();
+        
+        s.setServiceDescription(service);
+        s.setPrice(price);
+        return s;
+        
+        
     }
 
 }
