9a8093d35c6a9fd971bc83f6341420480b7fd28e
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-06 20:41:07 -0500

    Chapter 3 additional

--- src/chapter3/FixDebugThree1.java
+++ src/chapter3/FixDebugThree1.java
@@ -1,18 +1,21 @@
 // This class calculates a waitperson's tip as 15% of the bill
-public class DebugThree1
-{
-   public static void main(String args[])
-   {
+
+package chapter3;
+
+public class FixDebugThree1{
+   public static void main(String[] args){
       double myCheck = 50.00;
       double yourCheck = 19.95;
       System.out.println("Tips are");
+      
       calcTip(myCheck);
-      calctip(yourCheck);
+      calcTip(yourCheck);
     }
-    public static int calcTip(int bill)
+    public static void calcTip(double bill)
     {
       final double RATE = 0.15;
-      tip = bill + RATE;
+      double tip;
+      tip = RATE * bill;
       System.out.println("The tip should be at least " + tip);
     }
 }
