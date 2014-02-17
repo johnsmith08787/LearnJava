9772fb26c681c50aae045434a511a2d139e35256
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-17 11:57:15 -0500

    Chapter 5 - Volunteer and Short Circuit files.

---
+++ src/chapter5/AssignVolunteer3.java
@@ -0,0 +1,42 @@
+package chapter5;
+
+import java.util.Scanner;
+        
+
+public class AssignVolunteer3{
+    public static void main(String[] args){
+        int donationType;
+        String volunteer;
+	String message;
+	final int CLOTHING_CODE=1;
+        final int OTHER_CODE=2;
+        final String CLOTHING_PRICER="Regina";
+        final String OTHER_PRICER="Marcos";;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What type of donation is this?");
+        System.out.print("Enter " + CLOTHING_CODE + " for clothing, " + OTHER_CODE + " for anything else... ");
+        donationType = input.nextInt();
+
+        if((donationType == CLOTHING_CODE)||(donationType == OTHER_CODE)) 
+            if(donationType == CLOTHING_CODE)
+            {
+                volunteer = CLOTHING_PRICER;
+                message = "a clothing donation";
+            }
+            else
+            {
+                volunteer = OTHER_PRICER;
+                message = "another donation type.";
+            }
+        else 
+        {
+            volunteer = "not available. Invalid selection";
+            message = "invalid";
+        }
+	
+	System.out.println("You entered " + donationType);
+        System.out.println("This is " + message);
+        System.out.println("The volunteer who will price this item is " + volunteer);
+    }
+}
---
+++ src/chapter5/ShortCircuitTestAnd.java
@@ -0,0 +1,33 @@
+/**
+ * File Name:   ShortCircuitTestAnd
+ * Written by:  Aaron Arlotti
+ * Written on:  Feb 16, 2014
+ * Class:       Intro to Programming - ONT 187
+ * Instructor:  Mark Forder
+ * Book page:   
+ */
+
+package chapter5;
+
+public class ShortCircuitTestAnd {
+
+    public static void main(String[] args) {
+        if(trueMethod() && falseMethod())
+            System.out.println("Both are true");
+        else
+            System.out.println("Both are not true");
+    }
+    
+    public static boolean trueMethod(){
+        System.out.println("True statement");
+        return true;
+    }
+    
+    public static boolean falseMethod(){
+        System.out.println("False statement");
+        return false;
+    }
+
+}
+
+
