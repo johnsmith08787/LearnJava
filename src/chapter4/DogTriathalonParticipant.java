8ec929a78182a4fa457d6eb71aac6d7990fdb3d1
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-19 09:48:43 -0500

    Chapter 4 files.

---
+++ src/chapter4/DogTriathalonParticipant.java
@@ -0,0 +1,16 @@
+
+/**
+ * File Name:   DogTriathalonParticipant
+ * Written by:  Aaron Arlotti
+ * Written on:  Feb 19, 2014
+ * Class:       Intro to Programming - ONT 187
+ * Instructor:  Mark Forder
+ * Book page:   
+ */
+
+package chapter4;
+
+public class DogTriathalonParticipant {
+    // TODO code here
+}
+
---
+++ src/chapter4/TestDogs.java
@@ -0,0 +1,20 @@
+/**
+ * File Name:   TestDogs
+ * Written by:  Aaron Arlotti
+ * Written on:  Feb 19, 2014
+ * Class:       Intro to Programming - ONT 187
+ * Instructor:  Mark Forder
+ * Book page:   
+ */
+
+package chapter4;
+
+public class TestDogs {
+
+    public static void main(String[] args) {
+        // TODO code here
+    }
+
+}
+
+
