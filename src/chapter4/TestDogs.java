3b9facb515098ff9c58bcc76a82ad12f7e04d8be
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-19 20:02:24 -0500

    Chapter 5 additional files.

--- src/chapter4/TestDogs.java
+++ src/chapter4/TestDogs.java
@@ -1,3 +1,5 @@
+
+
 /**
  * File Name:   TestDogs
  * Written by:  Aaron Arlotti
@@ -12,7 +14,13 @@ package chapter4;
 public class TestDogs {
 
     public static void main(String[] args) {
-        // TODO code here
+	    DogTriathlonParticipant dog1 = new DogTriathlonParticipant("Bowser", 2, 85, 89, 0);
+	    dog1.display();
+	    DogTriathlonParticipant dog2 = new DogTriathlonParticipant("Rush", 3, 78, 72, 80);
+	    dog2.display();
+	    DogTriathlonParticipant dog3 = new DogTriathlonParticipant("Ginger", 3, 90, 86, 72);
+	    dog3.display();
+        
     }
 
 }
---
+++ src/chapter5/DogTriathlonParticipant2.java
@@ -0,0 +1,43 @@
+
+/**
+ * File Name:   DogTriathalonParticipant
+ * Written by:  Aaron Arlotti
+ * Written on:  Feb 19, 2014
+ * Class:       Intro to Programming - ONT 187
+ * Instructor:  Mark Forder
+ * Book page:   
+ */
+
+package chapter5;
+
+import chapter4.*;
+
+public class DogTriathlonParticipant2 {
+    private final int NUM_EVENTS;
+    private static int totalCumulativeScore = 0;
+
+    private String name;
+    private int obedienceScore;
+    private int conformationScore;
+    private int agilityScore;
+    private int total;
+    private double avg;
+
+
+    public DogTriathlonParticipant2(String name, int numEvents, int score1, int score2, int score3){
+            this.name = name;
+            NUM_EVENTS = numEvents;
+            obedienceScore = score1;
+            conformationScore =score2;
+            agilityScore = score3;
+
+            total = obedienceScore + conformationScore + agilityScore;
+            avg = (double) total / NUM_EVENTS;
+            totalCumulativeScore = totalCumulativeScore + total;
+}
+
+    public void display(){
+            System.out.println(name + " participated in " + NUM_EVENTS + " events and has an average score of " + avg);
+            System.out.println("  " + name + " has a total score of " + total + " bringing the cumulative score to " + totalCumulativeScore);
+    }
+}
