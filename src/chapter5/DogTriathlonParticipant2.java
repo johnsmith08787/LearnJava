ee5a8194be9a63be001ddeb358ee1bdc6e96c32f
Aaron Arlotti <aaronarlotti@gmail.com>
2014-02-19 20:18:27 -0500

    Chapter 5 - You do it

--- src/chapter5/DogTriathlonParticipant2.java
+++ src/chapter5/DogTriathlonParticipant2.java
@@ -22,9 +22,34 @@ public class DogTriathlonParticipant2 {
     private int agilityScore;
     private int total;
     private double avg;
+    private boolean scoresAgree;
 
 
     public DogTriathlonParticipant2(String name, int numEvents, int score1, int score2, int score3){
+        int totalNot0 = 0;
+        if(score1 != 0)
+            totalNot0 = totalNot0 + 1;
+        if(score2 != 0)
+            totalNot0 = totalNot0 + 1;
+        if(score3 != 0)
+            totalNot0 = totalNot0 + 1;
+        
+        
+        // compare the number of events to the total of nonzero scores and set the boolean variable scoresAgree
+        if(scoresAgree)
+        {
+            obedienceScore = score1;
+            conformationScore = score2;
+            agilityScore = score3;
+        }
+        else
+        {
+            obedienceScore = 0;
+            conformationScore = 0;
+            agilityScore = 0;
+        }
+            
+        
             this.name = name;
             NUM_EVENTS = numEvents;
             obedienceScore = score1;
@@ -32,12 +57,18 @@ public class DogTriathlonParticipant2 {
             agilityScore = score3;
 
             total = obedienceScore + conformationScore + agilityScore;
-            avg = (double) total / NUM_EVENTS;
+            if(NUM_EVENTS == 0)
+                avg = 0;
+            else
+                avg =(double) total / NUM_EVENTS;
             totalCumulativeScore = totalCumulativeScore + total;
 }
 
     public void display(){
             System.out.println(name + " participated in " + NUM_EVENTS + " events and has an average score of " + avg);
             System.out.println("  " + name + " has a total score of " + total + " bringing the cumulative score to " + totalCumulativeScore);
+            if(!scoresAgree)
+                System.out.println("Notice! Number of events for " + name + "does not agree with the scores reported.");
+    
     }
 }
---
+++ src/chapter5/TestDogs2.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * File Name:   TestDogs
+ * Written by:  Aaron Arlotti
+ * Written on:  Feb 19, 2014
+ * Class:       Intro to Programming - ONT 187
+ * Instructor:  Mark Forder
+ * Book page:   
+ */
+
+package chapter5;
+
+import chapter4.*;
+
+public class TestDogs2 {
+
+    public static void main(String[] args) {
+	    DogTriathlonParticipant2 dog1 = new DogTriathlonParticipant2("Bowser", 2, 85, 89, 0);
+	    dog1.display();
+	    DogTriathlonParticipant2 dog2 = new DogTriathlonParticipant2("Rush", 3, 78, 72, 80);
+	    dog2.display();
+	    DogTriathlonParticipant2 dog3 = new DogTriathlonParticipant2("Ginger", 3, 90, 86, 72);
+	    dog3.display();
+        
+    }
+
+}
+
+
