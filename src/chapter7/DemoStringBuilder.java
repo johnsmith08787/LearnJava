bcad722ee9c39f118e7054265243d4fd8c9646ac
Aaron <aaronarlotti@gmail.com>
2014-03-14 01:17:06 -0400

    Chapter 7 - DemoStringBuilder - adding to a string with StringBuffer()

---
+++ src/chapter7/DemoStringBuilder.java
@@ -0,0 +1,30 @@
+package chapter7;
+
+/**
+ * Created by aaron on 3/14/14.
+ */
+public class DemoStringBuilder {
+
+    public static void main(String[] args)
+    {
+        StringBuilder str = new StringBuilder("singing");
+        print(str);
+
+        str.append(" in the dead of ");
+        print(str);
+
+        str.insert(0, "Black");
+        print(str);
+        str.insert(5, "bird ");
+        print(str);
+
+        str.append("night");
+        print(str);
+    }
+
+        public static void print(StringBuilder s)
+        {
+            System.out.println(s);
+        }
+    }
+
