9c03febd73e4faaa7276ab7939cac8c422c4eca7
Aaron <aaronarlotti@gmail.com>
2014-03-14 23:17:58 -0400

    Chapter 8 DemoArray3 - Using a for loop to show the results of the array.

---
+++ src/chapter8/DemoArray3.java
@@ -0,0 +1,18 @@
+package chapter8;
+
+/**
+ * Created by aaron on 3/14/14.
+ */
+public class DemoArray3 {
+
+    public static void main(String[] args)
+    {
+        double[] salaries = {6.25, 6.55, 10.25, 16.85};
+
+        System.out.println("Salaries one by one are:");
+
+        for(int m = 0; m <salaries.length; ++m)
+            System.out.println(salaries[m]);
+
+    }
+}
